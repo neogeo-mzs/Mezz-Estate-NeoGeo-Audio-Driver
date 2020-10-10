d3ff3721abeb134c9de4c302243009fc8905efac
GbaCretin <robertopltchivi@gmail.com>
2020-10-10 11:23:35 +0200

    Now the panning is actually set in FM channels

--- 68k/fix.h
+++ 68k/fix.h
@@ -6,9 +6,9 @@ extern u8 fix_print_palette;
 extern u8 cursor_vram_increment;
 
 void FIX_set_cursor(u8 x, u8 y);
-void FIX_print_char(char chr);
-void FIX_print_char_repeat(char chr, u16 times);
-void FIX_print_string(char* str);
+void FIX_print_char(const char chr);
+void FIX_print_char_repeat(const char chr, u16 times);
+void FIX_print_string(const char* str);
 void FIX_print_byte(u8 byte);
 void FIX_print_word(u16 word);
 void FIX_print_long(u32 lng);
