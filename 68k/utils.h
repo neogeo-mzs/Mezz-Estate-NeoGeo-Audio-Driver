567eac79c494a7c72a1bb6f55a10b6790eb3b563
StereoMimi <42147021+stereomimi@users.noreply.github.com>
2021-04-12 17:47:56 +0200

    Updated 68k program to allow you to select songs

--- 68k/utils.h
+++ 68k/utils.h
@@ -1,6 +1,9 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#define CLAMP(x, min, max)  (((x) > (max)) ? (max) : (((x) < (min)) ? (min) : (x)))
+#define WRAP(x, min, max)   ( (x)>=(max) ? (x)+(min)-(max) : ( ((x)<(min)) ? (x)+(max)-(min) : (x) ) )
+
 void __attribute__((optimize("O0"))) wait_loop(int loops);
 
 #endif
