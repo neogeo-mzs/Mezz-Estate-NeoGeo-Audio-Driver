5836769d1ba5d652bfbd6c0b7f941927295dc631
GbaCretin <robertopltchivi@gmail.com>
2021-04-12 17:47:56 +0200

    Updated 68k program to allow you to select songs

--- 68k/main.c
+++ 68k/main.c
@@ -13,18 +13,11 @@
 // bpset 18, 1, { print "CHK instruction" }
 // bpset 1C, 1, { print "TRAPV instruction" }
 
-const char TRACK_METADATA[(FIX_LAYER_COLUMNS_SAFE-2) * (FIX_LAYER_ROWS-6)] =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
-    " ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
-    " laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in"
-    " voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat"
-    " non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-
 const char* INSTRUCTION_BOX_STRING =
-{
-    "\x83\x80\x80\x80\x80\x80\x80\x80\x80\x80\x81""Deflemask Player\x82\x80\x80\x80\x80\x80\x80\x80\x80\x80\x84"
+{                                                
+    "\x83\x80\x80\x80\x80\x80\x80\x80\x80\x80\x81""MZS test program\x82\x80\x80\x80\x80\x80\x80\x80\x80\x80\x84"
     "\x87                                    \x87"
-    "\x87         \x88 Play      \x89 Stop         \x87"
+    "\x87  \x88 Play   \x89 Stop   \x8E\x8F Song Select  \x87"
     "\x87                                    \x87"
     "\x85\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x86"
 };
@@ -55,33 +48,18 @@ void print_gui()
 
     FIX_SetCursor(0, 0);
     FIX_PrintString(INSTRUCTION_BOX_STRING);
-    
+
     FIX_SetCursor(1, 6);
-    FIX_PrintStringEx(TRACK_METADATA, FIX_LAYER_COLUMNS_SAFE-1);
+    FIX_PrintString("Song: $");
 }   
 
 int main()
 {
 	BIOS_FixClear();
     print_gui();
-    
-    /*FIX_SetPalette(1);
-    PAL_ENTRY(0, 1) = WHITE;
-    for (u8 bank = 0; bank < 0x20; bank++)
-    {
-        FIX_SetCursor(bank & 0x10, bank & 0x0F);
-        FIX_PrintNibble(bank >> 4);
-        FIX_PrintNibble(bank & 0x0F);
 
-        FIX_PrintString(": ");
-
-        Z80_UCOM_BANK_TEST_Z3(bank);
-        Z80_UCOM_BANK_TEST_Z3(bank);
-
-        u8 reg_sound = *REG_SOUND;
-        FIX_PrintNibble(reg_sound >> 4);
-        FIX_PrintNibble(reg_sound & 0x0F);
-    }*/
+    const int SONG_COUNT = 128;
+    int selected_song = 0;
 
     while(1)
     {
@@ -90,15 +68,19 @@ int main()
         render_status.vblank_done = 0;
         render_status.logic_done  = 0;
 
+        if (BIOS_P1CHANGE->up)
+            selected_song = WRAP(selected_song+1, 0, SONG_COUNT);
+        if (BIOS_P1CHANGE->down)
+            selected_song = WRAP(selected_song-1, 0, SONG_COUNT);
+
         if (BIOS_P1CHANGE->A)
-            Z80_UCOM_PLAY_SONG(0);
+            Z80_UCOM_PLAY_SONG(selected_song);
         if (BIOS_P1CHANGE->B)
             Z80_UCOM_STOP();
 
-        // print data (should use MESS OUT to prevent tearing)
-        //FIX_SetCursor(1, 1); 
-        //FIX_SetPalette(1);
-        //FIX_PrintByte(*REG_SOUND);
+        FIX_SetCursor(8, 6);
+        FIX_PrintNibble(selected_song >> 4);
+        FIX_PrintNibble(selected_song & 0x0F);
 
         // wait for vblank
         PAL_ENTRY(0, 1) = DARKGREY;
