6040ffcf6acb610b129a720f2dfaf0e91aa3abd9
GbaCretin <robertopltchivi@gmail.com>
2021-11-13 12:32:24 +0100

    Implemented MLM_set_channel_volume_FM

--- z80/mlm_test_data.s
+++ z80/mlm_test_data.s
@@ -109,15 +109,15 @@ MLM_song_fm4:
 	dw MLM_song_instruments-MLM_header
 
 MLM_el_fm:
-	db $02,2 ; Set instrument to 2
-	db $05,$E0
+	db $02,2   ; Set instrument to 2
+	db $05,$F0 ; Set channel volume
 	db $80 | (8*3), 0 | (5 << 4) ; Play FM note C4 and wait 8*3 ticks
 	db $80 | (8*3), 2 | (5 << 4) ; Play FM note D4 and wait 8*3 ticks
 	
 	db $09                        ; - Jump to sub event list
 	dw MLM_sub_el_wait-MLM_HEADER ; /
 
-	db $05,$FF
+	db $05,$FF ; Set channel volume
 	db $80 | (8*3), 4 | (5 << 4) ; Play FM note E4 and wait 8*3 ticks
 	db $80 | (8*3), 5 | (5 << 4) ; Play FM note C4 and wait 8*3 ticks
 	db $00
