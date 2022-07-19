c87065c748f15a0249a7c4071a773b6785019e2b
StereoMimi <42147021+stereomimi@users.noreply.github.com>
2022-07-19 16:52:49 -0300

    fixed fade in/out

--- z80/fade.s
+++ z80/fade.s
@@ -1,43 +1,52 @@
 ; DOESN'T BACKUP REGISTERS  
 FADE_irq:
+    brk
     ; If the fade offset is 0, 
     ; the fade system is disabled.
     ld a,(FADE_offset)
     or a,a
     ret z
 
-    ld b,a
+    ld b,a  ; store fade ofs in b
     ld a,(master_volume)
-    add a,b
+    ld c,a  ; backup old mvol in c
+    add a,b ; new mvol is in a
 
-    ; if the result overflowed, clamp it
-    ; and clear the fade offset.
-    ; if the result underflowed, clamp it,
-    ; clear the fade offset and buffer a song stop.
-    ld b,a
-    bit 7,a
-    jr nz,FADE_irq_bit7_set
+    ; If FADE_offset is positive, check for overflow...
+    bit 7,b 
+    jp z,FADE_irq_check_overflow
 
-    jr nc,FADE_irq_cend ; - B7: 0; CARRY: 0 (  +2 + +4 = +6   OK)
-
-    xor a,a
-    ld (FADE_offset),a
-    ld b,255            ; \
-    jr FADE_irq_cend    ; - B7: 0; CARRY: 1 (+254 + +4 = +2   NOT OK, SHOULD BE 255)
-FADE_irq_bit7_set:
-    jr c,FADE_irq_cend  ; - B7: 1; CARRY: 1 (+254 + -4 = +250 OK)
-
-    ld (do_stop_song),a ; since b7 is 1, a is guaranteed to not be 0
-    xor a,a
-    ld (FADE_offset),a
-
-    ld b,0              ; \
-    jr FADE_irq_cend    ; - B7: 1; CARRY: 0 (  +2 + -4 = +254 NOT OK, SHOULD BE 0)
+    ; ...Else, check for underflow.
+    or a,a ; cp a,0
+    jp z,FADE_irq_solve_underflow  ; mvol has reached 0.
+    cp a,c                         ; new_mvol >= old_mvol. (with a negative 
+    jp nc,FADE_irq_solve_underflow ; offset it means an underflow happened)
 
 FADE_irq_cend:
-    ld a,b
     ld (master_volume),a
-
     ld a,$FF 
     ld (do_reset_chvols),a
     ret
+
+FADE_irq_solve_underflow:
+    ; Solve underflow (or do nothing if mvol was 0),
+    ; then disable fade out and set do_stop_song flag.
+    ld a,255
+    ld (do_stop_song),a
+    xor a,a ; clear a
+    ld (FADE_offset),a
+    jp FADE_irq_cend
+
+FADE_irq_check_overflow:
+    cp a,255
+    jp z,FADE_irq_solve_overflow ; mvol has reached 255... (fade in needs to be disabled)
+    cp a,c                       
+    jp nc,FADE_irq_cend          ; if new_mvol >= old_mvol, no overflow has accured. 
+
+FADE_irq_solve_overflow:
+    ; solve overflow (or change nothing if 
+    ; mvol was 255), then disable fade in.
+    xor a,a ; clear a
+    ld (FADE_offset),a
+    dec a ; a becomes 255
+    jp FADE_irq_cend
